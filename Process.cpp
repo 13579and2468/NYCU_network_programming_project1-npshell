ad08385799c34bf5325d0bfda71ff1f4d297341c
a13579and2468 <a13579and2468@a13579and2468-X555LD>
2021-10-11 18:04:12 +0800

    ordinary pipe, builtin-commands,unknown commands, file redirection, lack number pipe

--- Process.cpp
+++ Process.cpp
@@ -1,13 +1,68 @@
 #include "Process.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <iostream>
+#include <exception>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+using namespace std;
 
 Process::Process(char* executable,char** argv)
 {
     this->executable = executable;
     this->argv = argv;
+    this->input = -1;  // -1 means no change
+    this->output = -1;
+    this->err = -1;
 }
 
 int Process::run()
 {
-    return execvp(executable,argv);
+    if(this->input!=-1)
+    {
+        dup2(this->input,STDIN_FILENO);
+        close(this->input);
+    }
+    if(this->output!=-1)
+    {
+        dup2(this->output,STDOUT_FILENO);
+        close(this->output);
+    }
+    if(this->err!=-1)
+    {
+        dup2(this->err,STDERR_FILENO);
+        close(this->err);
+    }
+
+    for (int i=0;argv[i]!=NULL;i++)
+    {
+        if(strcmp(argv[i],">")==0)
+        {
+            std::cout<<"file\n";
+            int fd;
+            try{
+                fd = open( argv[i+1],O_CREAT|O_RDWR|O_TRUNC,S_IRWXU|S_IRWXG|S_IRWXO );
+            }
+            catch(...){
+                std::cerr<< "open file failed\n";
+                exit(0);
+            }
+            dup2(fd,STDOUT_FILENO);
+            close(fd);
+            argv[i]=NULL;
+            break;
+        }
+    }
+    int r = 0;
+    if((r = execvp(executable,argv))==-1)
+    {
+        std::cerr<<"Unknown command: ["<<executable<<"].\n";
+        exit(0);
+    }
+    return r;
 }
--- npshell.cpp
+++ npshell.cpp
@@ -3,6 +3,9 @@
 #include <sstream>
 #include <algorithm>
 #include <unistd.h>
+#include <csignal>
+#include <sys/types.h> 
+#include <sys/wait.h>
 #include "Process.h"
 using namespace std;
 
@@ -33,6 +36,8 @@ int mysetenv(string var,string value){
 
 void init(){
   mysetenv("PATH","bin:.");
+  //setting SIGCHILD tp ignore will give zombie to init process
+  signal(SIGCHLD,SIG_IGN);
 }
 
 int main() {
@@ -41,7 +46,7 @@ int main() {
   int line=0;
   while(true)
   {
-    cout<<"%";
+    cout<<"% ";
     getline(cin,input);
     vector<string> tokens = split(input);
     if(tokens.size() == 0)continue;  // continue when empty line
@@ -67,9 +72,13 @@ int main() {
 
     //run commands
     auto it = tokens.begin();
+    auto previt = it;
+    vector<pid_t> childs;
+    int prevpipe[2]={-1,-1};
+    int thispipe[2]={-1,-1};
     while(true)
     {
-      auto previt = it;
+      // all commands between pipe
       if((it = find(it,tokens.end(),"|"))!=tokens.end())
       {
         // convert partial string vector into char**
@@ -81,39 +90,72 @@ int main() {
         }
         argv[i] = NULL;
         
+        // prepare pipe
+        close(prevpipe[0]);
+        close(prevpipe[1]);
+        prevpipe[0] = thispipe[0];
+        prevpipe[1] = thispipe[1];
+        if (pipe(thispipe)){
+          fprintf (stderr, "Pipe failed.\n");
+          return EXIT_FAILURE;
+        }
         // fork child and execute 
         pid_t pid = fork();
+
         if(pid == 0)
         {
           Process proc((char *)(*previt).c_str(),(char **)argv);
+          close(prevpipe[1]);
+          close(thispipe[0]);
+          proc.input = prevpipe[0];
+          proc.output = thispipe[1];
           proc.run();
         }else if(pid > 0)
         {
+          childs.push_back(pid);
           delete argv;
         }
         it++;
         previt = it;
-      }else
+      }else  // the last command of a line
       {
         char **argv = new char* [distance(previt,it)+1];
         int i=0;
         for(auto theit=previt;theit!=it;theit++)
         {
           argv[i++] = (char *)(*theit).c_str();
-          cout<<argv[i-1];
         }
         argv[i] = NULL;
         
+        close(prevpipe[0]);
+        close(prevpipe[1]);
+        prevpipe[0] = thispipe[0];
+        prevpipe[1] = thispipe[1];
         // fork child and execute 
         pid_t pid = fork();
         if(pid == 0)
         {
+          
           Process proc((char *)(*previt).c_str(),(char **)argv);
+          close(prevpipe[1]);
+          proc.input = prevpipe[0];
           proc.run();
         }else if(pid > 0)
         {
+          close(thispipe[0]);
+          close(thispipe[1]);
           delete argv;
+          childs.push_back(pid);
         }
+
+        //wait all process which needed
+        for (auto p : childs)
+        {
+          waitpid(p,NULL,0);
+        }
+
+        // next line
+        break;
       }
     }
     
